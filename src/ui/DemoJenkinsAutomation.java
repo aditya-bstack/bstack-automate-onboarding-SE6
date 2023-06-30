e58b7d4b3cd00675128f883f4cdc65c33d03eac6
aditya-bstack <aditya.gu@browserstack.com>
2023-06-30 09:52:18 +0530

    Adding Chrome Test2

--- src/ui/DemoJenkinsAutomation.java
+++ src/ui/DemoJenkinsAutomation.java
@@ -4,7 +4,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 
+import org.json.JSONObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
@@ -23,93 +25,123 @@ public class DemoJenkinsAutomation {
 	
 	@Test
 	public void chromeTest1() throws MalformedURLException {
-		
 		String demo_username = "demouser";
 		String demo_password = "testingisfun99";
 		MutableCapabilities capabilities = new MutableCapabilities();
 		capabilities.setCapability("browserName", "Chrome");
-		capabilities.setCapability("browserVersion", "100.0");
+		capabilities.setCapability("browserVersion", "103.0");
 		HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
 		browserstackOptions.put("os", "Windows");
-		browserstackOptions.put("osVersion", "10");
+		browserstackOptions.put("osVersion", "11");
 		browserstackOptions.put("sessionName", "BStack Build Name: " + buildName);
 		browserstackOptions.put("seleniumVersion", "4.0.0");
 		capabilities.setCapability("bstack:options", browserstackOptions);
 		driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
-		Assert.assertEquals(bstackdemoTestUtil(demo_username, demo_password), 1);
+		String ret = bstackdemoTestUtil(demo_username, demo_password);
+		Assert.assertEquals(ret, "success");
+		final JavascriptExecutor jse = (JavascriptExecutor) driver;
+		JSONObject executorObject = new JSONObject();
+		JSONObject argumentsObject = new JSONObject();
+		argumentsObject.put("status", "<passed/failed>");
+		argumentsObject.put("reason", "<reason>");
+		executorObject.put("action", "setSessionStatus");
+		executorObject.put("arguments", argumentsObject);
+		jse.executeScript(String.format("browserstack_executor: %s", executorObject));
+		if(ret.equals("success")) {
+			System.out.println("Chrome Test1 Passed");
+			jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Successfully placed order\"}}");
+		}
+		else {
+			System.out.println("Chrome Test1 Failed");
+		    jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \""+ret+"\"}}");
+
+		}
+	}
+	
+	@Test
+	public void chromeTest2() throws MalformedURLException {
+		String demo_username = "demouser";
+		String demo_password = "testingisfun99";
+		MutableCapabilities capabilities = new MutableCapabilities();
+		capabilities.setCapability("browserName", "Chrome");
+		capabilities.setCapability("browserVersion", "111.0");
+		HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
+		browserstackOptions.put("os", "OS X");
+		browserstackOptions.put("osVersion", "Monterey");
+		browserstackOptions.put("sessionName", "BStack Build Name: " + buildName);
+		browserstackOptions.put("seleniumVersion", "4.0.0");
+		capabilities.setCapability("bstack:options", browserstackOptions);
+		driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
+		String ret = bstackdemoTestUtil(demo_username, demo_password);
+		Assert.assertEquals(ret, "success");
+		final JavascriptExecutor jse = (JavascriptExecutor) driver;
+		JSONObject executorObject = new JSONObject();
+		JSONObject argumentsObject = new JSONObject();
+		argumentsObject.put("status", "<passed/failed>");
+		argumentsObject.put("reason", "<reason>");
+		executorObject.put("action", "setSessionStatus");
+		executorObject.put("arguments", argumentsObject);
+		jse.executeScript(String.format("browserstack_executor: %s", executorObject));
+		if(ret.equals("success")) {
+			System.out.println("Chrome Test2 Passed");
+			jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Successfully placed order\"}}");
+		}
+		else {
+			System.out.println("Chrome Test2 Failed");
+		    jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \""+ret+"\"}}");
+
+		}
 	}
 	
 	
-	public int bstackdemoTestUtil(String demo_username, String demo_password) {
-		driver.get("https://bstackdemo.com/");
-		driver.manage().window().maximize();
-		driver.findElement(By.id("signin")).click();
-		synchronized (driver){
-			try {
-				driver.wait(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		driver.findElement(By.xpath("/html/body/div/div[2]/div/form/div[2]/div[1]/div/div[1]")).click();
-		driver.findElement(By.id("react-select-2-input")).sendKeys(demo_username, Keys.ENTER);
-		driver.findElement(By.xpath("/html/body/div/div[2]/div/form/div[2]/div[2]/div/div[1]")).click();
-		driver.findElement(By.id("react-select-3-input")).sendKeys(demo_password, Keys.ENTER);
-		driver.findElement(By.id("login-btn")).click();
-		synchronized (driver){
-			try {
-				driver.wait(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+	public String bstackdemoTestUtil(String demo_username, String demo_password) {
 		try {
-			String logged_in_user = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div/div/div[2]/span")).getText();
-			if(!logged_in_user.equals(demo_username)) {
-				System.out.println("Invalid Credentials");
-				return 0;
+			driver.get("https://bstackdemo.com/");
+			driver.manage().window().maximize();
+			driver.findElement(By.id("signin")).click();
+			synchronized (driver){
+				driver.wait(2000);
+			}
+			driver.findElement(By.xpath("/html/body/div/div[2]/div/form/div[2]/div[1]/div/div[1]")).click();
+			driver.findElement(By.id("react-select-2-input")).sendKeys(demo_username, Keys.ENTER);
+			driver.findElement(By.xpath("/html/body/div/div[2]/div/form/div[2]/div[2]/div/div[1]")).click();
+			driver.findElement(By.id("react-select-3-input")).sendKeys(demo_password, Keys.ENTER);
+			driver.findElement(By.id("login-btn")).click();
+			synchronized (driver){
+				driver.wait(2000);
 			}
 			
-		} catch(Exception e) {
-			System.out.println("Invalid Credentials");
-			return 0;
-		}
-		driver.findElement(By.xpath("/html/body/div/div/div/main/div[2]/div[2]/div[4]")).click();
-		synchronized (driver){
-			try {
-				driver.wait(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			String logged_in_user = driver.findElement(By.xpath("/html/body/div/div/div/div[1]/div/div/div[2]/span")).getText();
+			if(!logged_in_user.equals(demo_username)) {
+				return "Invalid Credentials";
 			}
-		}
-		driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div[2]/div[3]/div[3]")).click();
-		synchronized (driver){
-			try {
+				
+			driver.findElement(By.xpath("/html/body/div/div/div/main/div[2]/div[2]/div[4]")).click();
+			synchronized (driver){
 				driver.wait(2000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
 			}
-		}
-		driver.findElement(By.id("firstNameInput")).sendKeys("sample_first_name");
-		driver.findElement(By.id("lastNameInput")).sendKeys("sample_last_name");
-		driver.findElement(By.id("addressLine1Input")).sendKeys("sample_address");
-		driver.findElement(By.id("provinceInput")).sendKeys("sample_province");
-		driver.findElement(By.id("postCodeInput")).sendKeys("sample_postal_code");
-		driver.findElement(By.id("checkout-shipping-continue")).click();
-		synchronized (driver){
-			try {
+			driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div[2]/div[3]/div[3]")).click();
+			synchronized (driver){
+				driver.wait(2000);
+			}
+			driver.findElement(By.id("firstNameInput")).sendKeys("sample_first_name");
+			driver.findElement(By.id("lastNameInput")).sendKeys("sample_last_name");
+			driver.findElement(By.id("addressLine1Input")).sendKeys("sample_address");
+			driver.findElement(By.id("provinceInput")).sendKeys("sample_province");
+			driver.findElement(By.id("postCodeInput")).sendKeys("sample_postal_code");
+			driver.findElement(By.id("checkout-shipping-continue")).click();
+			synchronized (driver){
 				driver.wait(5000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
 			}
+			String str = driver.findElement(By.id("confirmation-message")).getText();
+			if(str.contains("successful")) {
+				return "success";
+			}
+			return "error occurred";
 		}
-		String str = driver.findElement(By.id("confirmation-message")).getText();
-		int ret_val = 0;
-		if(str.contains("successful")) {
-			ret_val = 1;
+		catch (Exception e) {
+			return e.toString();
 		}
-		return ret_val;
-		
 	}
 	
 	@AfterMethod
