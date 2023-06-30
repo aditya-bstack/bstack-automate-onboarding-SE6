979df1fe189a55902b2b5d5ec27e6ec34df2b76f
aditya-bstack <aditya.gu@browserstack.com>
2023-06-30 10:07:40 +0530

    adding wait

--- src/ui/DemoJenkinsAutomation.java
+++ src/ui/DemoJenkinsAutomation.java
@@ -100,6 +100,9 @@ public class DemoJenkinsAutomation {
 		try {
 			driver.get("https://bstackdemo.com/");
 			driver.manage().window().maximize();
+			synchronized (driver){
+				driver.wait(5000);
+			}
 			driver.findElement(By.id("signin")).click();
 			synchronized (driver){
 				driver.wait(2000);
